b9b935eb8b6b6d59090224afebd3040813a6c916
zongjingyao <zongjy@yinnut.com>
2016-06-06 14:04:30 +0800

    Let BarcodeScannerView implement LifecycleEventListener. Add LifecycleEventListener in createViewInstance and remove LifecycleEventListener in onDropViewInstance.

--- android/src/main/java/com/eguma/barcodescanner/BarcodeScannerManager.java
+++ android/src/main/java/com/eguma/barcodescanner/BarcodeScannerManager.java
@@ -2,22 +2,17 @@ package com.eguma.barcodescanner;
 
 import android.view.View;
 
-import javax.annotation.Nullable;
-
-import com.facebook.react.bridge.LifecycleEventListener;
-import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.ViewGroupManager;
 import com.facebook.react.uimanager.annotations.ReactProp;
 
-public class BarcodeScannerManager extends ViewGroupManager<BarcodeScannerView> implements LifecycleEventListener {
+import javax.annotation.Nullable;
+
+public class BarcodeScannerManager extends ViewGroupManager<BarcodeScannerView> {
     private static final String REACT_CLASS = "RNBarcodeScannerView";
 
     private static final String DEFAULT_TORCH_MODE = "off";
     private static final String DEFAULT_CAMERA_TYPE = "back";
-
-    private BarcodeScannerView mScannerView;
-    private boolean mScannerViewVisible;
 
     @Override
     public String getName() {
@@ -40,31 +35,21 @@ public class BarcodeScannerManager extends ViewGroupManager<BarcodeScannerView> 
 
     @Override
     public BarcodeScannerView createViewInstance(ThemedReactContext context) {
-        context.addLifecycleEventListener(this);
-        mScannerView = new BarcodeScannerView(context);
-        mScannerView.setCameraType(DEFAULT_CAMERA_TYPE);
-        mScannerView.setFlash(DEFAULT_TORCH_MODE.equals("on"));
-        mScannerViewVisible = true;
-        return mScannerView;
-    }
-
-    @Override
-    public void onHostResume() {
-        mScannerView.onResume();
-    }
-
-    @Override
-    public void onHostPause() {
-        mScannerView.onPause();
-    }
-
-    @Override
-    public void onHostDestroy() {
-        mScannerView.stopCamera();
+        BarcodeScannerView scannerView = new BarcodeScannerView(context);
+        scannerView.setCameraType(DEFAULT_CAMERA_TYPE);
+        scannerView.setFlash(DEFAULT_TORCH_MODE.equals("on"));
+        context.addLifecycleEventListener(scannerView);
+        return scannerView;
     }
 
     @Override
     public void addView(BarcodeScannerView parent, View child, int index) {
         parent.addView(child, index + 1);   // index 0 for camera preview reserved
     }
+
+    @Override
+    public void onDropViewInstance(BarcodeScannerView view) {
+        ((ThemedReactContext) view.getContext()).removeLifecycleEventListener(view);
+    }
+
 }
